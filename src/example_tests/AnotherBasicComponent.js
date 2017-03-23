485ce2301047822bfab1f3c1d5e378908ad1a1bc
Jacob Lewis <jacob@reax.io>
2017-03-23 00:51:27 -0500

    Added additional dummy test component.

---
+++ src/example_tests/AnotherBasicComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class AnotherBasicComponent extends React.Component {
+  render() {
+    return(
+      <div className="another-basic-component">another basic component</div>
+    );
+  }
+}
+
+export default AnotherBasicComponent;
