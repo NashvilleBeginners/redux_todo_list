b441f35c4631dab055cca122a0d5a0f142319dec
Jacob Lewis <jacob@reax.io>
2017-03-22 23:44:46 -0500

    Added additional unit test example.

--- src/utils/capitalizeFirst-spec.js
+++ src/utils/capitalizeFirst-spec.js
@@ -3,7 +3,17 @@ import capitalizeFirst from './capitalizeFirst';
 
 describe('capitalizeFirst utility function', () => {
   it('should capitalize the first letter of a word', () => {
-    const name = "amanda";
+    const name = 'amanda';
     expect(capitalizeFirst(name)).to.equal('Amanda');
+  });
+
+  it('should handle one letter strings', () => {
+    const letter = 'a';
+    expect(capitalizeFirst(letter)).to.equal('A');
+  });
+
+  it('should handle empty strings', () => {
+    const letter = '';
+    expect(capitalizeFirst(letter)).to.equal('');
   });
 });
